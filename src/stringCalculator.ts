d7933d007a6f34904ff00219a31cee59174b34e5
Parth Patel <parth1020patel@gmail.com>
2024-10-17 16:37:47 +0530

    adding support for delimeters

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,11 +1,26 @@
 export class StringCalculator {
     add(numbers: string): number {
       if (numbers === "") return 0;
-      
-      // Split by comma or newline
-      const delimiters = /[,\n]/;
-      const nums = numbers.split(delimiters).map(Number);
-      
+  
+      let delimiterRegex = /[,\n]/;
+      let numsString = numbers;
+  
+      // Support custom delimiters
+      if (numbers.startsWith("//")) {
+        const delimiterEndIdx = numbers.indexOf("\n");
+        const delimiter = numbers.substring(2, delimiterEndIdx);
+        delimiterRegex = new RegExp(`[${delimiter}]`);
+        numsString = numbers.substring(delimiterEndIdx + 1);
+      }
+  
+      const nums = numsString.split(delimiterRegex).map(Number);
+  
+      // Check for negative numbers
+      const negatives = nums.filter(num => num < 0);
+      if (negatives.length > 0) {
+        throw new Error(`Negative numbers not allowed: ${negatives.join(", ")}`);
+      }
+  
       // Sum all numbers
       return nums.reduce((sum, num) => sum + num, 0);
     }
