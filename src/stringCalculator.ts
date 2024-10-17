dcd48e85a7e33ea759b1c4dea5fc6ccd8f93f4bb
Parth Patel <parth1020patel@gmail.com>
2024-10-17 16:30:04 +0530

    Adding basic add function in StringCalculator class

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,1 +1,13 @@
-// source code file
+export class StringCalculator {
+    add(numbers: string): number {
+      if (numbers === "") return 0;
+      
+      // Split by comma or newline
+      const delimiters = /[,\n]/;
+      const nums = numbers.split(delimiters).map(Number);
+      
+      // Sum all numbers
+      return nums.reduce((sum, num) => sum + num, 0);
+    }
+  }
+  
